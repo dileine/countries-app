a842bca0fa2de6430533c92e24910668065422f0
Mar LM <mar.audiovisuals@gmail.com>
2024-10-16 17:44:50 +0200

    Add Bootstrap for styling and layout; imported in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/routes";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <AppRoutes />
-      </div>
+      <AppRoutes />
     </BrowserRouter>
   );
 }
