c222f7e2f0249251d28087e4419a5c8e0df901c9
Mar LM <mar.audiovisuals@gmail.com>
2024-10-16 19:18:54 +0200

    Import index.css in app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/routes";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "././styles/index.css";
 
 function App() {
   return (
