3b8d5b67b914af367d8ebc538dcd346fad984c4d
Mar LM <mar.audiovisuals@gmail.com>
2024-10-16 17:46:15 +0200

    Integrate React-Bootstrap into CountriesList component and add CSS for uniform image sizing

--- src/components/CuntriesList/CountriesList.jsx
+++ src/components/CuntriesList/CountriesList.jsx
@@ -1,19 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Card, Row, Col } from "react-bootstrap";
 
 const CountriesList = ({ countries }) => {
   return (
     <div>
       <h2>Countries</h2>
-      <ul>
+      <Row>
         {countries.map((country) => (
-          <li key={country.cca3}>
-            <Link to={`/country/${country.name.common}`}>
-              {country.name.common}
-            </Link>
-          </li>
+          <Col xs={12} sm={6} md={4} lg={3} key={country.cca3} className='mb-2'>
+            <Card>
+              <Card.Body>
+                <Link to={`/country/${country.name.common}`}>
+                  <Card.Img
+                    variant='top'
+                    className='card-img-top'
+                    src={country.flags.png}
+                  />
+                  <Card.Title>{country.name.common}</Card.Title>
+                </Link>
+              </Card.Body>
+            </Card>
+          </Col>
         ))}
-      </ul>
+      </Row>
     </div>
   );
 };
